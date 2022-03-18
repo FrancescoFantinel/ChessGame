74da711d4069ceacdde33a3580d0a19ce7d74109
Francesco Fantinel <francescoacf@outlook.com>
2022-03-17 22:13:36 -0300

    Melhorando a implementacao do metodo imprimirTabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -4,10 +4,23 @@ using xadrez;
 using xadrez_console;
 using xadrez_console.xadrez;
 
-PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+try
+{
+    Tabuleiro tab = new Tabuleiro(8, 8);
 
-Console.WriteLine(pos);
+    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0,0));
+    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+    tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-Console.WriteLine(pos.toPosicao());
+    tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+    tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(4, 4));
+    tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(1, 2));
+
+    Tela.imprimirTabuleiro(tab);
+}
+catch (TabuleiroException e)
+{
+    Console.WriteLine(e.Message);
+}
 
 Console.ReadLine();
--- xadrez-console/xadrez/Rei.cs
+++ xadrez-console/xadrez/Rei.cs
@@ -12,7 +12,7 @@ namespace xadrez_console.xadrez
 
         public override string ToString()
         {
-            return "R ";
+            return "R";
         }
     }
 }

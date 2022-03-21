1f90a368fd292f2d49b6cb67edd3ca7278bbaa1f
Francesco Fantinel <francescoacf@outlook.com>
2022-03-21 20:06:46 -0300

    Testes iniciais com movimentos possiveis das pecas

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -13,8 +13,16 @@ try
         Console.Clear();
         Tela.imprimirTabuleiro(partida.tab);
 
+        Console.WriteLine();
+
         Console.Write("Origem: ");
         Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+        bool [,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+        
+        Console.Clear();
+        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
         Console.Write("Destino: ");
         Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
